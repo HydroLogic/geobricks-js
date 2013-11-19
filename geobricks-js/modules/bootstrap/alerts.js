566f879c8e919e3a03ecb1a073151ed46cc1788d
victorzinho <victor.gonzalez@geomati.co>
2013-11-19 09:14:02 +0100

    Add different directories for external libraries inside geobricks-js/lib. Move alerts-bootstrap to a 'bootstrap' directory as suggested in #1

---
+++ geobricks-js/modules/bootstrap/alerts.js
@@ -0,0 +1,36 @@
+define([ "jquery", "message-bus", "css-loader", "module", "layout", "bootstrap" ], function($, bus, cssLoader, module, layout) {
+	var config = module.config();
+
+	var severities = [ "success", "info", "warning", "danger" ];
+	var prefixes = {
+		success : config.successPrefix ? config.successPrefix : "",
+		info : config.infoPrefix ? config.infoPrefix : "",
+		warning : config.warningPrefix ? config.warningPrefix : "",
+		danger : config.dangerPrefix ? config.dangerPrefix : ""
+	};
+
+	var divContainer = $("#" + layout.alerts);
+
+	bus.listen("alert", function(event, msg, severity) {
+		if ($.inArray(severity, severities) == -1) {
+			severity = severities[severities.length - 1];
+		}
+
+		console.log(msg);
+		console.log(severity);
+
+		var button = $("<button/>").addClass("close");
+		button.attr("data-dismiss", "alert");
+		button.text("&times");
+		var text = $("<div><strong>" + prefixes[severity] + "</strong> " + msg + "</div>");
+		var div = $("<div/>").addClass("alert alert-dismissable alert-" + severity);
+
+		div.append(button);
+		div.append(text);
+		divContainer.append(div);
+
+		setTimeout(function() {
+			div.alert("close");
+		}, 5000);
+	});
+});
