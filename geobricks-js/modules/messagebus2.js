a768879000b7fd6d74872d9b4d085b8b58dbebb2
Oscar Fonts <oscar.fonts@gmail.com>
2013-12-11 18:15:00 +0100

    message-bus, v.2 & 3. Proposals for #5

---
+++ geobricks-js/modules/messagebus2.js
@@ -0,0 +1,44 @@
+/**
+ * Message Bus v.2
+ * ---------------
+ *
+ * Changes from v.1:
+ * 
+ *  - Uses jQuery's .on() method, instead of .bind()
+ *  - Public methods are renamed to: publish, subscribe and unsubcribe
+ *  - Message "name" is now "channel"; "parameters" renamed to "message"
+ *  - Optional subscription parameter to set callback's context
+ *  - The "event" parameter disappears from callback signature,
+ *    callback receives a single param: function callback(message)
+ *  - New unsubscribe method, to stop listening to messages
+ */
+define(["jquery"], function($) {
+    var o = {}; // Single object where all events are attached.
+    
+    // Publish a message in a channel
+    function publish(channel, message) {
+        $(o).trigger(channel, message);
+    }
+    
+    // Subscribe to a channel.
+    // Provide a callback to be run after a message is published.
+    // Callback function receives a single param: the "message".
+    // Optionally, provide a context for the callback execution (*this*).
+    function subscribe(channel, callback, context) {
+        $(o).on(channel, function(event, message) {
+            callback.apply(context, message);
+        });
+    }
+    
+    // Remove an existing subscription.
+    // Provide a channel and the callback function to be unregistered.
+    function unsubscribe(channel, callback) {
+        $(o).off(channel, callback);
+    }
+    
+    return {
+        publish: publish,
+        subscribe: subscribe,
+        unsubscribe: unsubscribe
+    };
+});
